4054cbd97286c6d99de5f86915f2d8574aefcddd
mrkavian <mehrad.kavian@gmail.com>
2018-05-14 13:35:14 +0430

    just finished the war game

--- CodeWithChrisTour/CodeWithChrisTour/ViewController.swift
+++ CodeWithChrisTour/CodeWithChrisTour/ViewController.swift
@@ -11,12 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var rightImageView: UIImageView!
-    @IBOutlet weak var leftImageView : UIImageView!
-    
-    @IBOutlet weak var leftScoreLabel: UILabel!
     @IBOutlet weak var rightScoreLabel: UILabel!
     
+    var rightScore = 0
+   
+    @IBOutlet weak var leftImageView : UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
     
+    var leftScore = 0
+    
+    let cardNames = ["card2","card3","card4","card5","card6","card7","card8","card9","card10","jack","queen","king","ace"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,15 +34,35 @@ class ViewController: UIViewController {
     
     @IBAction func dealTapped (sender: UIButton) {
         
-        //randomize right number from 2 to 10
-        let rightRandNo = arc4random_uniform(9) + 2
+        //randomize right number from 0 to 12
+        let rightRandNo = Int(arc4random_uniform(13))
         
-        rightImageView.image = UIImage(named: "card\(rightRandNo)")
+        rightImageView.image = UIImage(named: cardNames[rightRandNo])
         
-        let leftRandNo = arc4random_uniform(9) + 2
-        //randomize left number from 2 to 10
+        //randomize left number from 0 to 12
+        let leftRandNo = Int(arc4random_uniform(13))
         
-        leftImageView.image = UIImage(named: "card\(leftRandNo)")
+        
+        leftImageView.image = UIImage(named: cardNames[leftRandNo])
+        
+        if leftRandNo > rightRandNo {
+            //left card wins
+            
+            leftScore += 1
+            
+            leftScoreLabel.text = String(leftScore)
+        }
+        
+        else if rightRandNo == leftRandNo {
+            //it's a tie
+        }
+        
+        else {
+            //right card wins
+            rightScore += 1
+            
+            rightScoreLabel.text = String(rightScore)
+        }
         
     }
 }
--- SwiftPG.playground/Contents.swift
+++ SwiftPG.playground/Contents.swift
@@ -4,10 +4,19 @@ import UIKit
 
 var str = "Hello, playground"
 
-var randNo = arc4random_uniform(13)
+var a = 10
 
-print("card\(randNo)")
+var b = 5
+var c = 1
 
+if a < b ||  a==c {
+    
+}
+    
+else if a==b {
+    
+}
 
-
-
+else {
+    
+}
